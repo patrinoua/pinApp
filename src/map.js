1acb12098f6e4a739aa9d7606fab6d01fad23138
angeliki <patrinoua@gmail.com>
2018-05-09 19:47:50 +0200

    i hope this works

---
+++ src/map.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+const mapStateToProps = function(state) {
+    return {
+        onlineUsers: state.onlineUsers
+    };
+};
+class MapApp extends React.Component {
+    render() {
+        return <h1> Welcome to map!</h1>;
+    }
+}
+
+export default connect(mapStateToProps)(MapApp);
---
+++ src/navigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { Logo, Login } from "./welcome";
+import { ProfilePic } from "./profile";
+// import { OnlineUsers } from "./onlineUsers";
+import MapApp from "./map";
+import Chat from "./chat";
+
+export default function Navigation(props) {
+    return (
+        <div id="navigation">
+            <Logo />
+            <Link to="/user"> Hello {props.first}</Link>
+            <a href="/logout"> Logout </a>
+            <Link to="/onlineUsers"> Online </Link>
+            <Link to="/friends"> Friends </Link>
+            <Link to="/chat"> Chat </Link>
+            <Link to="/map"> MapApp </Link>
+            <ProfilePic {...props} />
+        </div>
+    );
+}
