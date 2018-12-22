736ed07f5987d5abc6bc2baf84bb665c3e3ba962
Mona Lisa <patrinoua@gmail.com>
2018-12-23 00:53:28 +0200

    lint app.js

--- src/app.js
+++ src/app.js
@@ -1,210 +1,184 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Link, HashRouter, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import { Welcome, Logo, Login } from "./welcome";
-import axios from "./axios";
-import { ProfilePage, ProfilePic, UploadProfilePic, EditBio } from "./profile";
-import { Navigation } from "./navigation";
-import OtherProfilePage from "./otherProfile";
-import Friends from "./friends";
-import { composeWithDevTools } from "redux-devtools-extension";
-import OnlineUsers from "./onlineUsers";
-import Chat from "./chat";
-import MapContainer from "./mapcontainer";
-import { saveUserInfo } from "./actions";
-import PinClick from "./PinClick.js";
-'use strict'
-// import { EditBio , ExistingBio } from './bio';
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import {connect} from 'react-redux'
+import axios from './axios'
+import {ProfilePage, UploadProfilePic} from './profile'
+import {Navigation} from './navigation'
+import OtherProfilePage from './otherProfile'
+import Friends from './friends'
+import OnlineUsers from './onlineUsers'
+import MapContainer from './mapcontainer'
+import {saveUserInfo} from './actions'
+import PinClick from './PinClick.js'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        // this.showUploader = this.showUploader.bind(this);
-        this.changeImage = this.changeImage.bind(this);
-        this.hideUploader = this.hideUploader.bind(this);
-        this.toggleUploader = this.toggleUploader.bind(this);
-        this.fileToUpload = {};
-        this.state.toggleUploader = false;
-        this.changeInputValues = this.changeInputValues.bind(this);
-        this.togglePinClick = this.togglePinClick.bind(this);
-    }
-    togglePinClick() {
-        location.replace("/");
-    }
-    toggleUploader() {
-        this.setState({
-            toggleUploader: !this.state.toggleUploader
-        });
-    }
-    // showUploader() {
-    //     this.setState({
-    //         uploaderIsVisible: true
-    //     });
-    // }
-    changeImage(img) {
-        this.setState({
-            profilepic: img
-        });
-    }
-    changeInputValues(inputValues) {
-        let { id, first, last, email, bio } = inputValues;
-        this.setState({
-            id,
-            first,
-            last,
-            email,
-            bio
-        });
-    }
-    hideUploader() {
-        this.setState({
-            uploaderIsVisible: false
-        });
-    }
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.changeImage = this.changeImage.bind(this)
+    this.hideUploader = this.hideUploader.bind(this)
+    this.toggleUploader = this.toggleUploader.bind(this)
+    this.fileToUpload = {}
+    this.state.toggleUploader = false
+    this.changeInputValues = this.changeInputValues.bind(this)
+    this.togglePinClick = this.togglePinClick.bind(this)
+  }
+  togglePinClick() {
+    location.replace('/')
+  }
+  toggleUploader() {
+    this.setState({
+      toggleUploader: !this.state.toggleUploader
+    })
+  }
+  changeImage(img) {
+    this.setState({
+      profilepic: img
+    })
+  }
+  changeInputValues(inputValues) {
+    let {id, first, last, email, bio} = inputValues
+    this.setState({
+      id,
+      first,
+      last,
+      email,
+      bio
+    })
+  }
+  hideUploader() {
+    this.setState({
+      uploaderIsVisible: false
+    })
+  }
 
-    componentDidMount() {
-        navigator.geolocation.getCurrentPosition((position) => {
-            this.setState({
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            });
-        });
-        axios.get("/getUser").then((response) => {
-            if (response.data.success) {
-                this.props.dispatch(saveUserInfo(response.data.user));
-                this.setState(response.data.user);
-            } else {
-                console.log(
-                    "response.data in getUser had an error ",
-                    response.data
-                );
-            }
-        });
-    }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      })
+    })
+    axios.get('/getUser').then(response => {
+      if (response.data.success) {
+        this.props.dispatch(saveUserInfo(response.data.user))
+        this.setState(response.data.user)
+      } else {
+        console.log('response.data in getUser had an error ', response.data)
+      }
+    })
+  }
 
-    render() {
-        return (
-            <div className="routeContainer">
-                <BrowserRouter>
-                    <div className="appContainer">
-                        <Navigation
-                            {...this.state}
-                            toggleUploader={this.toggleUploader}
-                            makeUploaderVisible={this.showUploader}
-                            hideUploader={this.hideUploader}
-                            userMenuPopUpStatus={this.state.toggleUserMenu}
-                        />
+  render() {
+    return (
+      <div className="routeContainer">
+        <BrowserRouter>
+          <div className="appContainer">
+            <Navigation
+              {...this.state}
+              toggleUploader={this.toggleUploader}
+              makeUploaderVisible={this.showUploader}
+              hideUploader={this.hideUploader}
+              userMenuPopUpStatus={this.state.toggleUserMenu}
+            />
 
-                        <Route
-                            exact
-                            path="/profile"
-                            render={() => (
-                                <ProfilePage
-                                    {...this.state}
-                                    toggleUploader={this.toggleUploader}
-                                    makeUploaderVisible={this.showUploader}
-                                    hideUploader={this.hideUploader}
-                                    changeInputValues={this.changeInputValues}
-                                    changeImage={this.changeImage}
-                                />
-                            )}
-                        />
+            <Route
+              exact
+              path="/profile"
+              render={() => (
+                <ProfilePage
+                  {...this.state}
+                  toggleUploader={this.toggleUploader}
+                  makeUploaderVisible={this.showUploader}
+                  hideUploader={this.hideUploader}
+                  changeInputValues={this.changeInputValues}
+                  changeImage={this.changeImage}
+                />
+              )}
+            />
 
-                        <Route path="/friends" component={Friends} />
+            <Route path="/friends" component={Friends} />
 
-                        <Route
-                            exact
-                            path="/user/:id"
-                            render={(x) => (
-                                <React.Fragment>
-                                    <OtherProfilePage
-                                        lat={this.state.lat}
-                                        lng={this.state.lng}
-                                        match={x.match}
-                                        history={x.history}
-                                        id={this.state.id}
-                                    />
-                                </React.Fragment>
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/pin/:encryptedPinId"
-                            render={(x) => (
-                                <PinClick
-                                    match={x.match}
-                                    history={x.history}
-                                    togglePinClick={this.togglePinClick}
-                                    flag={true}
-                                />
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/sharedpin/:encryptedPinId"
-                            render={(x) => (
-                                <PinClick
-                                    match={x.match}
-                                    history={x.history}
-                                    togglePinClick={this.togglePinClick}
-                                    flag={true}
-                                />
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/onlineUsers"
-                            component={OnlineUsers}
-                        />
-                        {/*<Route exact path="/chat" component={Chat} />*/}
-                        <Route
-                            exact
-                            path="/"
-                            render={() => (
-                                <MapContainer
-                                    lat={this.state.lat}
-                                    lng={this.state.lng}
-                                    {...this.state}
-                                />
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/map"
-                            render={() => (
-                                <MapContainer
-                                    lat={this.state.lat}
-                                    lng={this.state.lng}
-                                    {...this.state}
-                                />
-                            )}
-                        />
-                    </div>
-                </BrowserRouter>
+            <Route
+              exact
+              path="/user/:id"
+              render={x => (
+                <React.Fragment>
+                  <OtherProfilePage
+                    lat={this.state.lat}
+                    lng={this.state.lng}
+                    match={x.match}
+                    history={x.history}
+                    id={this.state.id}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route
+              exact
+              path="/pin/:encryptedPinId"
+              render={x => (
+                <PinClick
+                  match={x.match}
+                  history={x.history}
+                  togglePinClick={this.togglePinClick}
+                  flag={true}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/sharedpin/:encryptedPinId"
+              render={x => (
+                <PinClick
+                  match={x.match}
+                  history={x.history}
+                  togglePinClick={this.togglePinClick}
+                  flag={true}
+                />
+              )}
+            />
+            <Route exact path="/onlineUsers" component={OnlineUsers} />
+            {/*<Route exact path="/chat" component={Chat} />*/}
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <MapContainer
+                  lat={this.state.lat}
+                  lng={this.state.lng}
+                  {...this.state}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/map"
+              render={() => (
+                <MapContainer
+                  lat={this.state.lat}
+                  lng={this.state.lng}
+                  {...this.state}
+                />
+              )}
+            />
+          </div>
+        </BrowserRouter>
 
-                {this.state.toggleUploader && (
-                    <UploadProfilePic
-                        changeImage={this.changeImage}
-                        hideUploader={this.hideUploader}
-                        toggleUploader={this.toggleUploader}
-                    />
-                )}
-                {/*{this.state.toggleUserMenu &&
-                    (<UserMenuPopUp
-                    toggleUserMenu={this.toggleUserMenu}
-                    userMenuPopUpStatus={this.state.toggleUserMenu}
-                    />)
-                }*/}
-            </div>
-        );
-    }
+        {this.state.toggleUploader && (
+          <UploadProfilePic
+            changeImage={this.changeImage}
+            hideUploader={this.hideUploader}
+            toggleUploader={this.toggleUploader}
+          />
+        )}
+      </div>
+    )
+  }
 }
 const mapStateToProps = function(state) {
-    return {
-        markersArray: state.markersArray
-        // pins: state.onlineUsers
-    };
-};
-export default connect(mapStateToProps)(App);
+  return {
+    markersArray: state.markersArray
+  }
+}
+export default connect(mapStateToProps)(App)
